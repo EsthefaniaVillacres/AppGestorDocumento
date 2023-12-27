4080e2b3861c0b6466a4f29a1a7c99c7080a6aa9
EsthefaniaVillacres <84297019+EsthefaniaVillacres@users.noreply.github.com>
2023-12-27 11:28:51 -0500

    creacion de modulos de carreras

---
+++ app-gestordocumentos/src/app/admin/modulos/career-admin/career-admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CareerAdminRoutingModule } from './career-admin-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CareerAdminRoutingModule
+  ]
+})
+export class CareerAdminModule { }
