6f7558bd5f17cd7c990e23361925cc298372dbc3
EsthefaniaVillacres <84297019+EsthefaniaVillacres@users.noreply.github.com>
2023-12-27 11:12:23 -0500

    creacion modeo carreras

---
+++ api-gestordocumentos/app/Models/Career.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Career extends Model
+{
+    use HasFactory;
+    protected $table = "career";
+    protected $primaryKey='Id';
+    protected $fillable=['Codigo','Nombre','Estado'];
+    public $timestamps=false;
+}
