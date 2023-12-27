056163e73a4b864ee5ee28362a8102f86a1d573c
EsthefaniaVillacres <84297019+EsthefaniaVillacres@users.noreply.github.com>
2023-12-27 09:37:50 -0500

    Creacion bd y migracion

---
+++ api-gestordocumentos/database/migrations/2023_12_27_142119_create_faculty_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faculty', function (Blueprint $table) {
+            $table->id('Id');
+            $table->string('Codigo',15)->unique();
+            $table->string('Nombre',150);
+            $table->string('Estado',15)->default('Activo'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faculty');
+    }
+};
---
+++ api-gestordocumentos/database/migrations/2023_12_27_143107_create_manage_faculty_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manage_faculty', function (Blueprint $table) {
+            $table->id('Id');
+            $table->unsignedBigInteger('IdUser');
+            $table->unsignedBigInteger('IdFaculty');
+            $table->string('Estado',15)->default('Activo'); 
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manage_faculty');
+    }
+};
